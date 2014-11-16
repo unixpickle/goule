449a2673bf2084e6a771c292faa2e6def078d076
Alex Nichol <unixpickle@gmail.com>
2014-11-16 14:42:36 -0500

    started working on web mockup

--- src/source_url.go
+++ src/source_url.go
@@ -11,9 +11,17 @@ type SourceURL struct {
 	Path     string `json:"path"`
 }
 
+// MatchesURL returns true if the receiving SourceURL matches a specified URL.
 func (self SourceURL) MatchesURL(url *url.URL) bool {
 	if url.Scheme != self.Protocol || url.Host != self.Hostname {
 		return false
 	}
 	return strings.HasPrefix(url.Path, self.Path)
+}
+
+// SubpathForURL returns the path components that a given URL contains that the
+// receiving SourceURL does not.
+// This subpath can be appended to the proxy destination in some cases.
+func (self SourceURL) SubpathForURL(url *url.URL) string {
+	return url.Path[len(self.Path) : len(url.Path)]
 }
