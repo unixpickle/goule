89bc531323d3ed048172fa48e58328cc55231878
Alex Nichol <unixpickle@gmail.com>
2014-11-17 06:43:58 -0500

    serve static admin files (insecurely)

--- src/admin_handler.go
+++ src/admin_handler.go
@@ -2,17 +2,30 @@ package goule
 
 import (
 	"net/http"
+	"runtime"
 	"strings"
+	pathlib "path"
 )
 
 func AdminHandler(res http.ResponseWriter, req *http.Request, path string) {
+	if path == "/" || path == "" {
+		AdminHandler(res, req, "/index.html")
+		return
+	}
+	
 	if strings.HasPrefix(path, "/api/") {
 		apiName := path[5 : len(path)]
 		AdminAPICall(res, req, apiName)
 		return
 	}
-	res.Header().Set("Content-Type", "text/plain")
-	res.Write([]byte("Welcome to the admin site! Path:" + path))
+	
+	// TODO: here, validate the path before serving the file; this is a security
+	// concern.
+	
+	// Serve static file
+	_, filename, _, _ := runtime.Caller(1)
+	actualPath := pathlib.Join(pathlib.Dir(filename), "../static" + path)
+	http.ServeFile(res, req, actualPath)
 }
 
 func AdminAPICall(res http.ResponseWriter, req *http.Request, api string) {
