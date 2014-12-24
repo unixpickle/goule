122b27e52394189501f39d19699864bb39dc301b
Alex Nichol <unixpickle@gmail.com>
2014-12-24 15:48:20 -0500

    created goule-config utility

--- api.go
+++ api.go
@@ -9,13 +9,14 @@ import (
 	"strings"
 )
 
+// SessionIdCookie is the cookie name for the Goule session ID.
 const SessionIdCookie = "goule_id"
 
 // APIHandler handles an API HTTP request.
 func (g *Goule) APIHandler(w http.ResponseWriter, r *http.Request) {
 	// The path is "/api/APINAME"
 	api := r.URL.Path[5:]
-	
+
 	// Make sure they are authorized to make this request.
 	authed := g.checkAuth(w, r)
 	if !authed && api != "Auth" {
@@ -23,28 +24,28 @@ func (g *Goule) APIHandler(w http.ResponseWriter, r *http.Request) {
 			errors.New("You must authenticate."))
 		return
 	}
-	
+
 	// Read the contents of the request
 	contents, err := gohttputil.ReadRequest(r, 0x10000)
 	if err != nil {
 		gohttputil.RespondJSON(w, http.StatusBadRequest, err.Error())
 		return
 	}
-	
+
 	// Run the call
 	values, code, err := g.APICall(api, contents)
 	if err != nil {
 		gohttputil.RespondJSON(w, code, err)
 		return
 	}
-	
+
 	// The "Auth" call is special--it creates a new cookie.
 	if api == "Auth" && values[0].(bool) {
 		id := g.sessions.login()
 		cookie := &http.Cookie{Name: SessionIdCookie, Value: id}
 		http.SetCookie(w, cookie)
 	}
-	
+
 	gohttputil.RespondJSON(w, http.StatusOK, values)
 }
 
@@ -55,19 +56,19 @@ func (g *Goule) APICall(name string, body []byte) ([]interface{}, int, error) {
 	if !method.IsValid() {
 		return nil, http.StatusNotFound, errors.New("Unknown API: " + name)
 	}
-	
+
 	// Decode the raw arguments.
 	var rawArgs []string
 	if err := json.Unmarshal(body, &rawArgs); err != nil {
 		return nil, http.StatusBadRequest, err
 	}
-	
+
 	// Decode the exact arguments.
 	args, err := decodeArgs(method, rawArgs)
 	if err != nil {
 		return nil, http.StatusBadRequest, err
 	}
-	
+
 	// Run the call
 	var res []reflect.Value
 	if strings.HasPrefix(name, "Set") || name == "Auth" {
@@ -79,7 +80,7 @@ func (g *Goule) APICall(name string, body []byte) ([]interface{}, int, error) {
 		res = method.Call(args)
 		g.mutex.RUnlock()
 	}
-	
+
 	// Convert the return value to an array of serializable objects.
 	resList := make([]interface{}, len(res))
 	for i, val := range res {
@@ -90,7 +91,7 @@ func (g *Goule) APICall(name string, body []byte) ([]interface{}, int, error) {
 		}
 		resList[i] = rawValue
 	}
-	
+
 	return resList, 0, nil
 }
 
@@ -124,7 +125,7 @@ func decodeArgs(method reflect.Value, raw []string) ([]reflect.Value, error) {
 	if method.Type().NumIn() != len(raw) {
 		return nil, errors.New("Invalid number of arguments.")
 	}
-	
+
 	// Decode each argument separately.
 	res := make([]reflect.Value, len(raw))
 	for i, rawArg := range raw {
@@ -135,6 +136,6 @@ func decodeArgs(method reflect.Value, raw []string) ([]reflect.Value, error) {
 		}
 		res[i] = reflect.Indirect(dec)
 	}
-	
+
 	return res, nil
 }
