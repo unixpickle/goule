7bb2f7ff8e6ff997b6dc4bcd81dcfb729a4c1732
Alex Nichol <unixpickle@gmail.com>
2015-03-18 15:04:47 -0400

    modify rules in page

--- assets/scripts/rules.js
+++ assets/scripts/rules.js
@@ -1,11 +1,39 @@
 (function() {
 
+  function addRule() {
+    var container = $('#rules');
+    container.append(createRuleElement('', ['']));
+  }
+
   function createRuleElement(host, targets) {
+    var element = $('<div />', {class: 'rule'});
+    var input = $('<input />', {
+      value: host,
+      placeholder: 'Host'
+    });
+    var remove = $('<button>Remove</button>');
+    var add = $('<button>Add Target</button>');
+    element.append(input);
+    element.append(remove);
+    element.append(add);
+    remove.click(function() {
+      element.remove();
+    });
+    add.click(function() {
+      element.append(createTargetElement(''));
+    });
+    for (var i = 0, len = targets.length; i < len; ++i) {
+      element.append(createTargetElement(targets[i]));
+    }
+    return element;
   }
 
   function createTargetElement(hostname) {
     var element = $('<div />', {class: 'target'});
-    var input = $('<input />', {value: hostname});
+    var input = $('<input />', {
+      value: hostname,
+      placeholder: 'Target'
+    });
     var remove = $('<button>Remove</button>');
     element.append(input);
     element.append(remove);
@@ -16,17 +44,26 @@
   }
 
   function loadRules(rules) {
+    // Get a sorted list of hosts.
+    var hosts = [];
     for (var key in rules) {
       if (!rules.hasOwnProperty(key)) {
         continue;
       }
+      hosts.push(key);
+    }
+    hosts.sort();
+    // Add an element for each host.
+    var container = $('#rules');
+    for (var i = 0, len = hosts.length; i < len; ++i) {
+      var key = hosts[i];
       var targets = rules[key];
       var element = createRuleElement(key, targets);
+      container.push(element);
     }
   }
 
-  
-
+  window.app.addRule = addRule;
   window.app.loadRules = loadRules;
 
 })();
