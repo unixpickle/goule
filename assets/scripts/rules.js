07496d49a1c05dd24f83d8173989f0ae5521dde4
Alex Nichol <unixpickle@gmail.com>
2015-07-08 23:43:22 -0400

    clean up rules code

--- assets/scripts/rules.js
+++ assets/scripts/rules.js
@@ -1,48 +1,41 @@
 (function() {
 
   function addRule() {
-    var container = $('#rules');
-    container.append(createRuleElement('', ['']));
+    $('#rules').append(createRuleElement('', ['']));
   }
 
   function createRuleElement(host, targets) {
-    var element = $('<div></div>', {class: 'rule'});
-    var input = $('<input />', {
+    var $element = $('<div></div>', {class: 'rule'});
+    var $input = $('<input></input>', {
       value: host,
       placeholder: 'Host',
       class: 'host-name'
     });
-    var remove = $('<button>Remove</button>');
-    var add = $('<button>Add Target</button>');
-    element.append(input);
-    element.append(remove);
-    element.append(add);
-    remove.click(function() {
-      element.remove();
+    var $remove = $('<button>Remove</button>').click(function() {
+      $element.remove();
     });
-    add.click(function() {
-      element.append(createTargetElement(''));
+    var $add = $('<button>Add Target</button>').click(function() {
+      $element.append(createTargetElement(''));
     });
+    $element.append($input, $remove, $add);
     for (var i = 0, len = targets.length; i < len; ++i) {
-      element.append(createTargetElement(targets[i]));
+      $element.append(createTargetElement(targets[i]));
     }
-    return element;
+    return $element;
   }
 
   function createTargetElement(hostname) {
-    var element = $('<div></div>', {class: 'target'});
-    var input = $('<input></input>', {
+    var $element = $('<div></div>', {class: 'target'});
+    var $input = $('<input></input>', {
       value: hostname,
       placeholder: 'Target',
       class: 'target-name'
     });
-    var remove = $('<button>Remove</button>');
-    element.append(input);
-    element.append(remove);
-    remove.click(function() {
-      element.remove();
+    var $remove = $('<button>Remove</button>').click(function() {
+      $element.remove();
     });
-    return element;
+    $element.append($input, $remove);
+    return $element;
   }
 
   function loadRules(rules) {
@@ -56,30 +49,30 @@
     }
     hosts.sort();
     // Add an element for each host.
-    var container = $('#rules');
+    var $container = $('#rules');
     for (var i = 0, len = hosts.length; i < len; ++i) {
       var key = hosts[i];
       var targets = rules[key];
-      var element = createRuleElement(key, targets);
-      container.append(element);
+      var $element = createRuleElement(key, targets);
+      $container.append($element);
     }
   }
-  
+
   function save() {
     // Generate a rules object.
     var result = {};
-    var rules = $('.rule');
-    for (var i = 0, len = rules.length; i < len; ++i) {
-      var rule = $(rules[i]);
-      var name = rule.find('input.host-name').val();
-      var targets = rule.find('input.target-name');
+    var $rules = $('.rule');
+    for (var i = 0, len = $rules.length; i < len; ++i) {
+      var $rule = $rules.eq(i);
+      var name = $rule.find('input.host-name').val();
+      var targets = $rule.find('input.target-name');
       var targetNames = [];
       for (var j = 0, len1 = targets.length; j < len1; ++j) {
-        targetNames[j] = $(targets[j]).val();
+        targetNames[j] = targets.eq(j).val();
       }
       result[name] = targetNames;
     }
-    
+
     // Send the rules object to the server.
     var encoded = encodeURIComponent(JSON.stringify(result));
     window.location = '/setrules?rules=' + encoded;
